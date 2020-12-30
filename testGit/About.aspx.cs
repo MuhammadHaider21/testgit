67b58853c81e4d7667eb9502c73a10226556facf
HP <HP@DESKTOP-02JTRIE>
2020-12-30 13:54:03 +0500

    string

--- testGit/About.aspx.cs
+++ testGit/About.aspx.cs
@@ -11,7 +11,8 @@ namespace testGit
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            string name = "";
+            string nic = "";
         }
     }
 }
